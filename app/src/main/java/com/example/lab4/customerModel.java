57b896d8630a94923714506b05c6703e8019a264
“basmaalamoud”a <basma17y@gmail.com>
2023-04-11 19:18:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab4/customerModel.java
@@ -0,0 +1,44 @@
+package com.example.lab4;
+
+public class customerModel {
+    public customerModel(int ID, String s, int i, boolean checked) {
+        this.ID = ID;
+    }
+    @Override
+    public String toString() {
+        return "customerModel{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+    private int ID;
+    private String name;
+    private int age;
+    private boolean isActive;
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public boolean isActive() {
+        return isActive;
+    }
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
